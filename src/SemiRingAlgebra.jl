d839a11abcf9f30a56249fc390f14d0874d1d7e0
Viral B. Shah <viral@mayin.org>
2013-05-12 15:33:12 +0530

    Fix zero(T)

--- src/SemiRingAlgebra.jl
+++ src/SemiRingAlgebra.jl
@@ -12,8 +12,7 @@ show(io::IO, k::SRNumber) = print(io, k.val)
 +(a::SRNumber, b::SRNumber) = SRNumber(max(a.val,b.val))
 *(a::SRNumber, b::SRNumber) = SRNumber(a.val+b.val)
 
-zero{T}(x::SRNumber{T}) = SRNumber(zero(T))
-convert{T}(::Type{SRNumber{T}}, i::Int64) = SRNumber(convert(T,i))
+zero{T}(::Type{SRNumber{T}}) = SRNumber(zero(T))
 promote_rule(::Type{SRNumber}, ::Type{Integer}) = SRNumber
 promote_rule(::Type{SRNumber}, ::Type{FloatingPoint}) = SRNumber
 
