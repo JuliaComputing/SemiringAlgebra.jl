0030e893962b12f4e1ad766dac721a7f3726d2f3
Viral B. Shah <viral@mayin.org>
2013-05-12 15:43:21 +0530

    Fix spellings in filenames.

--- test/test.jl
+++ test/test.jl
@@ -1,7 +1,11 @@
+require("SemiringAlgebra.jl")
+import SemiringAlgebra.SRNumber
+
 A=map(SRNumber,rand(5,5))
-B=map(SRNumber,rand(5,5))
 
 println(A)
-println(B)
-println(A*B)
+println(A*A)
 
+se=speye(5)
+s = SparseMatrixCSC(s.m, s.n, s.colptr, s.rowval, map(SRNumber, rand(5)))
+println(s*s)
