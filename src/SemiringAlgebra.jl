8789c34003fdd71d1353829266561d55347a3d17
Viral B. Shah <viral@mayin.org>
2013-05-13 08:26:51 +0530

    Add *(plus, times) syntax for semiring_matmul

--- src/SemiringAlgebra.jl
+++ src/SemiringAlgebra.jl
@@ -20,4 +20,17 @@ promote_rule(::Type{MPNumber}, ::Type{FloatingPoint}) = MPNumber
 mparray(A::Array) = map(MPNumber, A)
 array{T}(A::Array{MPNumber{T}}) = map(x->x.val, A)
 
+function semiring_matmul(pl, ti, A::Matrix, B::Matrix)
+    m=size(A,1); n=size(B,2); p=size(A,2)
+    C=[ti(A[i,1],B[1,j]) for i=1:m,j=1:n]
+    for i=1:m, j=1:n, k=2:p
+        C[i,j] = pl(C[i,j], ti(A[i,k],B[k,j]))
+    end 
+    C         
+end      
+
+function *(pl::Function, ti::Function)
+   (A,B)->semiring_matmul(pl,ti,A,B)
+end
+
 end # module
