006402b5753f8921f74207200f02b9395e35184b
Viral B. Shah <ViralBShah@users.noreply.github.com>
2021-07-14 21:04:28 -0400

    Update test.jl

--- test/test.jl
+++ test/test.jl
@@ -3,7 +3,7 @@ importall Base.Test
 require("SemiringAlgebra.jl")
 importall SemiringAlgebra
 
-A = ones(3,3); A[:,3] = 3
+A = ones(3,3); A[:,3] .= 3
 Amp = mparray(A)
 @test array(Amp*Amp) == [4.0  4.0  6.0; 4.0  4.0  6.0; 4.0  4.0  6.0]
 @test *(max,+)(A,A) == [4.0  4.0  6.0; 4.0  4.0  6.0; 4.0  4.0  6.0]
